1c13762e2885b91f0efb0539e0009368265605bf
Kevin Daniel Taylor <kevinmusic123@gmail.com>
2024-11-22 22:47:45 -0300

    refact: DEV-001 | creating callbacks to reduce the amount of ifs

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,21 @@
 namespace Cooper {
 
   namespace Entity {
+  /**
+   * @description: Begin of Entity Component Systems Pattern...
+   * @resouce: https://www.umlboard.com/design-patterns/entity-component-system.html
+   */
 
   constexpr size_t MAX_COMPONENTS = 12;
+
+  /**
+   * Entity specification.
+   */
   typedef uint32_t Entity;
 
   class ComponentManager {
+    template <typename Component>
+    using ComponentStorage = std::unordered_map<Entity, Component>;
   public:
     template <typename Component>
     void add(Entity entity, Component component) {
@@ -18,27 +28,63 @@ namespace Cooper {
     }
 
     template <typename Component>
-    std::unordered_map<Entity, Component>& getComponentStorage() {
-        static std::unordered_map<Entity, Component> storage;
+    ComponentStorage<Component>& getComponentStorage() {
+        static ComponentStorage<Component> storage;
         return storage;
-    }    
-    
+    }
+
     template <typename Component>
     Component* get(Entity entity) {
-      auto& storage = getComponentStorage<Component>();
-      if (storage.find(entity) != storage.end()) {
-        return &storage[entity];
-      }
-      return nullptr;
+      ComponentStorage<Component>& storage = getComponentStorage<Component>();
+      return modifyComponentStorageByCondition<Component>(entity, storage, &foundComponent, &getComponent);
     }
 
     template <typename Component>
     void remove(Entity entity) {
-      std::unordered_map<Entity, Component>& storage = getComponentStorage<Component>();
-      if (storage.find(entity) != storage.end()) {
-        storage.erase(entity);
-      }
+      ComponentStorage<Component>& storage = getComponentStorage<Component>();
+      modifyComponentStorageByCondition<Component>(entity, storage, &foundComponent, &eraseComponent);
     }
+
+  private:
+    template <typename Component>
+    using FindComponentCondition = bool (*)(ComponentStorage<Component>&, Entity entity);
+
+    template <typename Component>
+    using ModifyComponentStorageCallback = Component* (*)(ComponentStorage<Component>&, Entity entity);
+
+    template <typename Component>
+    Component* modifyComponentStorageByCondition(Entity                                    entity, 
+                                                 ComponentStorage<Component>&              storage, 
+                                                 FindComponentCondition<Component>         condition,
+                                                 ModifyComponentStorageCallback<Component> callback)
+    {
+      if (condition(storage, entity)) {
+        return callback(storage, entity);
+      }
+      return nullptr;
+    }
+
+  private:
+    /**
+     * Begin Static callbacks...
+     */
+    template <typename Component>
+    static bool foundComponent(ComponentStorage<Component>& componentStorage, Entity entity) {
+      return (componentStorage.find(entity) != componentStorage.end());
+    }
+
+    template <typename Component>
+    static Component* eraseComponent(ComponentStorage<Component>& componentStorage, Entity entity) {
+      componentStorage.erase(entity);
+    }
+
+    template <typename Component>
+    static Component* getComponent(ComponentStorage<Component>& componentStorage, Entity entity) {
+      return &componentStorage[entity];
+    }
+    /**
+     * End Static callbacks...
+     */
   };
 
   class EntityManager {
@@ -112,45 +158,51 @@ public:
   } 
 private:
   void run() {
-    // Entity::EntityManager    entityManager;
-    // Entity::ComponentManager componentManager;
+    Entity::EntityManager    entityManager;
+    Entity::ComponentManager componentManager;
 
 
-    // for (int idx = 0; idx < 5000; idx++) {
-    //   Entity::Entity player = entityManager.create();
+    for (int idx = 0; idx < 5000; idx++) {
+      Entity::Entity player = entityManager.create();
 
-    //   entityManager.addComponent<Rectangle>(player);
-    //   entityManager.addComponent<Color>(player);
-    //   entityManager.addComponent<Vector2>(player);
+      entityManager.addComponent<Rectangle>(player);
+      entityManager.addComponent<Color>(player);
+      entityManager.addComponent<Vector2>(player);
 
-    //   componentManager.add(player, Rectangle{ (float) GetRandomValue(0, GetScreenWidth() - 50), (float) GetRandomValue(0, GetScreenHeight() - 50), 50.0f, 50.0f });
-    //   componentManager.add(player, Color{ (unsigned const char)GetRandomValue(0, 255), (unsigned const char)GetRandomValue(0, 255), (unsigned const char)GetRandomValue(0, 255), 255 });
-    //   componentManager.add(player, Vector2 { (float) GetRandomValue(-3, 3), (float) GetRandomValue(-3, 3)});
-    // }
+      componentManager.add(player, Rectangle{ (float) GetRandomValue(0, GetScreenWidth() - 50), (float) GetRandomValue(0, GetScreenHeight() - 50), 50.0f, 50.0f });
+      componentManager.add(player, Color{ (unsigned const char)GetRandomValue(0, 255), (unsigned const char)GetRandomValue(0, 255), (unsigned const char)GetRandomValue(0, 255), 255 });
+      componentManager.add(player, Vector2 { (float) GetRandomValue(-3, 3), (float) GetRandomValue(-3, 3)});
+    }
 
     while (!WindowShouldClose()) {
 
       BeginDrawing();
-        // for (int idx = 0; idx < 5000; idx++) {
-        //   Color*            color     = componentManager.get<Color>(idx);
-        //   Rectangle*        rectangle = componentManager.get<Rectangle>(idx);
-        //   Vector2*          speed     = componentManager.get<Vector2>(idx); 
+        /**
+         * BEGIN TESTS...
+         */
+        for (int idx = 0; idx < 5000; idx++) {
+          Color*     color     = componentManager.get<Color>(idx);
+          Vector2*   speed     = componentManager.get<Vector2>(idx); 
+          Rectangle* rectangle = componentManager.get<Rectangle>(idx);
 
-        //   if (rectangle && color && speed) {
-        //     DrawRectangle(rectangle->x, rectangle->y, rectangle->width, rectangle->height, *color);
+          if (rectangle && color && speed) {
+            DrawRectangle(rectangle->x, rectangle->y, rectangle->width, rectangle->height, *color);
 
-        //     rectangle->x += speed->x;
-        //     rectangle->y += speed->y;
+            rectangle->x += speed->x;
+            rectangle->y += speed->y;
 
-        //     if (rectangle->y > GetScreenHeight() - rectangle->height || rectangle->y < 0) {
-        //       speed->y *= -1;
-        //     }
+            if (rectangle->y > GetScreenHeight() - rectangle->height || rectangle->y < 0) {
+              speed->y *= -1;
+            }
 
-        //     if (rectangle->x> GetScreenWidth() - rectangle->width || rectangle->x < 0) {
-        //       speed->x *= -1;
-        //     }
-        //   }
-        // }
+            if (rectangle->x> GetScreenWidth() - rectangle->width || rectangle->x < 0) {
+              speed->x *= -1;
+            }
+          }
+        }
+        /**
+         * END TESTS...
+         */
         ClearBackground(GetColor(0x181818FF));
         DrawFPS(0, 0);
       EndDrawing();
