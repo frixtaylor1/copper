09f4888a7642cb75917bf517cc2276347fdcd7d7
Kevin Daniel Taylor <taylorfrix123@gmail.com>
2024-11-24 20:16:43 -0300

    refact: DEV-001 | remove files of dependency entt

--- Makefile
+++ Makefile
@@ -1,20 +1,25 @@
-build_dir              = ./build
-src_dir                = ./src
-dependencies_dir       = ./dependencies
-entt_dir               = $(dependencies_dir)/entt-3.14.0/src/entt/entity/
-graphics_dir           = $(dependencies_dir)/raylib
-graphics_lib_dir       = $(graphics_dir)/lib
-graphics_include_dir   = $(graphics_dir)/include
-compile_graphics_flags = -lGL -lm -lpthread -ldl -lrt -lX11
-graphics_command       = $(graphics_lib_dir)/libraylib.a -I$(graphics_include_dir) -L$(graphics_lib_dir) $(compile_graphics_flags) 
-entt_command           = -I$(entt_dir)
+build_dir               = ./build
+src_dir                 = ./src
+dependencies_dir        = ./dependencies
+entt_dir                = $(dependencies_dir)/entt-3.14.0/single_include/entt/
+graphics_dir            = $(dependencies_dir)/raylib
+graphics_lib_dir        = $(graphics_dir)/lib
+graphics_include_dir    = $(graphics_dir)/include
+compile_graphics_flags  = -lGL -lm -lpthread -ldl -lrt -lX11
+graphics_command        = $(graphics_lib_dir)/libraylib.a -I$(graphics_include_dir) -L$(graphics_lib_dir) $(compile_graphics_flags) 
+entt_command            = -I$(entt_dir)
 
-project_name           = game
-cxx_compiler           = g++
-cxx_version            = -std=c++17
-cxx_flags              = $(cxx_version) -Wall -Werror -pedantic
+project_name            = game
+cxx_compiler            = g++
+cxx_version             = -std=c++17
+cxx_flags               = $(cxx_version) -Wall -Werror -pedantic
 
-main_build_target      = $(build_dir)/$(project_name) 
+cxx_debug_compiler      = g++
+cxx_debug_flags         = -g 
+cxx_debug_command       = $(cxx_debug_compiler) $(cxx_debug_flags) 
+
+main_build_target       = $(build_dir)/$(project_name) 
+main_build_debug_target = $(build_dir)/$(project_name) 
 
 $(main_build_target): | $(build_dir) 
 	$(cxx_compiler) $(cxx_flags) -o $(build_dir)/$(project_name) $(src_dir)/main.cpp $(graphics_command) $(entt_command)
@@ -25,6 +30,9 @@ $(build_dir):
 
 clean:
 	rm -rf $(build_dir)
+
+debug: $(main_build_debug_target)
+	$(cxx_debug_command) -o $(build_dir)/$(project_name) $(src_dir)/main.cpp $(graphics_command) $(entt_command) && gdb $($build_dir)/$(project_name)
 
 run: $(main_build_target)
 	./$(main_build_target)
--- src/main.cpp
+++ src/main.cpp
@@ -1,137 +1,153 @@
 #include <raylib.h>
-#include <sys/types.h>
-#include <unordered_map>
-#include <bitset>
-#include <vector>
-#include <registry.hpp>
+#include <entt.hpp>
 
 namespace Cooper {
 
-  /**
-   * ECS Pattern...
-   * @description: Begin of Entity Component Systems Pattern...
-   * @resouce: https://www.umlboard.com/design-patterns/entity-component-system.html
-   */
-  namespace Entity {
-    namespace Components {
+/**
+ * @formatting: clang formatting style
+ * @resouce: https://www.umlboard.com/design-patterns/entity-component-system.html
+ * @dependency: Entt
+ */
+namespace Entity {
+namespace Components {
 
-    typedef Vector2 Speed2;
-    typedef Vector3 Speed3;
+typedef Vector2 Speed2;
 
-    struct Player {
-      Speed2    speed;
-      Rectangle rectangle;
-      Color     color;
-    };
+struct Movable {
+  Vector2 pos;
+};
 
-    }
+}
+
+namespace Systems {
+
+struct MovementSystem {
+  void update(entt::registry& reg) {
+    updatePlayer(reg);
   }
 
-  namespace Core {
+  typedef bool (*MovableCondition)(int key);
+  typedef void (*MovableAction)(Entity::Components::Movable& movable, const Entity::Components::Speed2);
+  static void playerActsByCondition(int                       key,
+                             Entity::Components::Movable&     movable,
+                             const Entity::Components::Speed2 speed,
+                             MovableCondition                 condition,
+                             MovableAction                    callback) {
+    if (IsKeyDown(key)) callback(movable, speed);
+  }
 
-  class Window {
-  public:
-    Window(int width = 1024,int height = 762, const char* title = "Cooper")
+  void updatePlayer(entt::registry& reg) {
+    auto playerView = reg.view<Entity::Components::Speed2, Entity::Components::Movable>();
+    playerView.each([](const Entity::Components::Speed2& speed, Entity::Components::Movable& movable) {
+      playerActsByCondition(KEY_W, movable, speed, &IsKeyDown, &movePlayerUp);
+      playerActsByCondition(KEY_S, movable, speed, &IsKeyDown, &movePlayerDown);
+      playerActsByCondition(KEY_A, movable, speed, &IsKeyDown, &movePlayerLeft);
+      playerActsByCondition(KEY_D, movable, speed, &IsKeyDown, &movePlayerRight);
+    });
+  }
+
+  static void movePlayerUp(Entity::Components::Movable& movable, const Entity::Components::Speed2 speed) {
+    movable.pos.y -= speed.y;
+  }
+
+  static void movePlayerDown(Entity::Components::Movable& movable, const Entity::Components::Speed2 speed) {
+    movable.pos.y += speed.y;
+  }
+
+  static void movePlayerLeft(Entity::Components::Movable& movable, const Entity::Components::Speed2 speed) {
+    movable.pos.x -= speed.x;
+  }
+
+  static void movePlayerRight(Entity::Components::Movable& movable, const Entity::Components::Speed2 speed) {
+    movable.pos.x += speed.x;
+  }
+};
+
+class RenderSystem {
+public:
+
+private:
+};
+
+} // namespace Systems
+
+} // namespace Entity
+
+namespace Core {
+
+class Window {
+public:
+  Window(int width = 1024, int height = 762, const char* title = "Cooper")
       : width(width), height(height), title(title) {
-      initialize();
-    }
-
-    ~Window() {
-      close();
-    }
-
-    static bool shouldNotClose() {
-      return !WindowShouldClose();
-    }
-
-  private:
-    void initialize() {
-      InitWindow(width, height, title);
-    }
-
-    void close() {
-      CloseWindow();
-    }
-
-  private:
-    int         width,
-                height;
-    const char* title;
-  };
-  
+    initialize();
   }
+
+  ~Window() {
+    close(); 
+  }
+
+  static bool shouldNotClose() {
+    return !WindowShouldClose();
+  }
+
+private:
+  void initialize() {
+    InitWindow(width, height, title);
+  }
+
+  void close() {
+    CloseWindow();
+  }
+
+private:
+  int         width,
+              height;
+  const char* title;
+};
+
+} // namespace Core
 
 class Game {
 public:
   Game() {
     initialize();
     run();
-  } 
+  }
+
 private:
   void run() {
     entt::registry reg;
     initializeEntities(reg);
+    Entity::Systems::MovementSystem movementSystem;
 
     while (window.shouldNotClose()) {
-
       BeginDrawing();
-        /**
-         * BEGIN TESTS...
-         */
-        auto view = reg.view<Entity::Components::Speed2, Rectangle, const Color>();
-        view.each([](Entity::Components::Speed2& speed, Rectangle& rectangle, const Color color) {
-          DrawRectangle(rectangle.x, rectangle.y, rectangle.width, rectangle.height, color);
-          rectangle.x += speed.x;
-          rectangle.y += speed.y;
+      movementSystem.update(reg);
 
-          if (rectangle.y > GetScreenHeight() - rectangle.height || rectangle.y < 0) {
-            speed.y *= -1;
-          }
-
-          if (rectangle.x> GetScreenWidth() - rectangle.width || rectangle.x < 0) {
-            speed.x *= -1;
-          }
-        });
-        /**
-         * END TESTS...
-         */
-        ClearBackground(GetColor(0x181818FF));
-        DrawFPS(0, 0);
+      auto drawView = reg.view<Entity::Components::Movable, Color>();
+      drawView.each([](const Entity::Components::Movable movable, const Color color) {
+        DrawRectangle(movable.pos.x, movable.pos.y, 50, 50, color);
+      });
+      ClearBackground(GetColor(0x181818FF));
+      DrawFPS(0, 0);
       EndDrawing();
     }
   }
 
-  void initialize() {
-    SetTargetFPS(60);
-  }
+  void initialize() { SetTargetFPS(60); }
 
   void initializeEntities(entt::registry& reg) {
-    for (size_t nbEntities = 0; nbEntities < 10000; nbEntities++) {
-      const auto entity = reg.create();
-      reg.emplace<Entity::Components::Speed2>(entity,
-        (float) GetRandomValue(-3, 3),
-        (float) GetRandomValue(-3, 3)
-      );
-      reg.emplace<Rectangle>(entity,
-        (float) GetRandomValue(0, GetScreenWidth() - 50),
-        (float) GetRandomValue(0, GetScreenHeight() - 50),
-        50.0f, 
-        50.0f 
-      );
-      reg.emplace<Color>(entity,
-        (unsigned const char) GetRandomValue(0, 255),
-        (unsigned const char) GetRandomValue(0, 255),
-        (unsigned const char) GetRandomValue(0, 255),
-        (unsigned const char) 255
-      );
-    }
+    entt::entity player = reg.create();
+    reg.emplace<Entity::Components::Speed2>(player, 6.0f, 6.0f);
+    reg.emplace<Entity::Components::Movable>(player, Vector2 { 10.f, 10.f });
+    reg.emplace<Color>(player, RED);
   }
 
 private:
-  Core::Window window {};
+  Core::Window window{};
 };
 
-}
+} // namespace Cooper
 
 int main(void) {
   Cooper::Game game;
