20bd7bd9e0ba17ced12854e0217ccda05fce9a4a
Kevin Daniel Taylor <taylorfrix123@gmail.com>
2024-11-28 22:02:21 -0300

    DEV-001 | adding/changing LICENSE README AUTHORS and trivial changes in main.cpp

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
-# copper
+---
+
+# **Copper** 
+_A game made in C++._
+
+## 🎮 **Features**
+- Built with **C++** for performance and flexibility.
+- Uses **Raylib** for stunning rendering.
+- Powered by **EnTT** for efficient ECS (Entity-Component-System).
+
+---
+
+## 🚀 **Installation**
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/tu-usuario/copper.git
+   cd copper
+   ```
+
+2. Install dependencies (ensure these are installed globally or in your project):
+   - [Raylib](https://github.com/raysan5/raylib)
+   - [EnTT](https://github.com/skypjack/entt)
+
+3. Build the project using your preferred build system (e.g., CMake):
+   ```bash
+   mkdir build && cd build
+   cmake ..
+   make
+   ```
+
+4. Run the game:
+   ```bash
+   ./copper
+   ```
+
+---
+
+## 📦 **Third-party Dependencies**
+
+- **[Raylib](https://github.com/raysan5/raylib):** A simple and easy-to-use library for rendering graphics, audio, and more.
+- **[EnTT](https://github.com/skypjack/entt):** A fast, header-only ECS library.
+
+---
+
+## 💡 **Contributing**
+
+1. Fork the repository.
+2. Create a new branch for your feature or bugfix:
+   ```bash
+   git checkout -b feature-name
+   ```
+3. Commit your changes:
+   ```bash
+   git commit -m "Add feature-name"
+   ```
+4. Push the branch:
+   ```bash
+   git push origin feature-name
+   ```
+5. Open a pull request!
+
+---
+
+## 📜 **License**
+This project is licensed underethe [GPLV3 License](LICENSE).
+
+---
+
+## 🛠 **Future Plans**
+...
+---
+
+## 🤝 **Contact**
+Feel free to reach out for questions or collaboration:
+**Author:** [Kevin Daniel Taylor](https://github.com/frixtaylor1)
+
+---
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,76 @@
+/**
+ * This file is part of Copper and is licensed under the GPLv3.
+ * See the LICENSE file for more details.
+ */
+
 #include <raylib.h>
 #include <entt.hpp>
+#include <assert.h>
+
+#define MUST_IMPLEMENT() assert(false && "Must Implement!")
 
 namespace Cooper {
-
 /**
- * @formatting: clang formatting style
- * @resouce: https://www.umlboard.com/design-patterns/entity-component-system.html
- * @dependency: Entt
+ * @formatting:
+ *  - clang(kinda) formatting style
+ *  - indent: 2 spaces
+ *
+ *  In order to understand the infrastructure of this project and the dependencies:
+ *    @resouce:
+ *      - https://www.umlboard.com/design-patterns/entity-component-system.html
+ *
+ * @dependencies: [ Entt, Raylib ]
  */
+
 namespace Entity {
 namespace Components {
 
 typedef Vector2 Speed2;
 
-struct Movable {
+struct Controllable {
   Vector2 pos;
+  Speed2  speed;
 };
 
 }
 
+namespace Commands {
+
+struct ICommand {
+  virtual ~ICommand() {}
+  virtual void execute(Components::Controllable& controllable) = 0;
+};
+
+struct MoveUpCommand : public ICommand {
+  void execute(Components::Controllable& controllable) {
+    controllable.pos.y -= controllable.speed.y;
+  }
+};
+
+struct MoveDownCommand : public ICommand {
+  void execute(Components::Controllable& controllable) {
+    controllable.pos.y += controllable.speed.y;
+  }
+};
+
+struct MoveLeftCommand : public ICommand {
+  void execute(Components::Controllable& controllable) {
+    controllable.pos.x -= controllable.speed.x;
+  }
+};
+
+struct MoveRightCommand : public ICommand {
+  void execute(Components::Controllable& controllable) {
+    controllable.pos.x += controllable.speed.x;
+  }
+};
+
+} // namespace Commands
+
 namespace Systems {
 
 struct ISystem {
-  virtual ~ISystem() {} 
+  virtual ~ISystem() {}
   virtual void update(entt::registry& reg) = 0;
 };
 
@@ -33,43 +81,32 @@ public:
   }
 
 private:
-  typedef bool (*MovableCondition)(int key);
-  typedef void (*MovableAction)(Entity::Components::Movable& movable, const Entity::Components::Speed2);
-  static void playerActsByCondition(int                       key,
-                             Entity::Components::Movable&     movable,
-                             const Entity::Components::Speed2 speed,
-                             MovableCondition                 condition,
-                             MovableAction                    callback) {
-    if (condition(key)) callback(movable, speed);
-  }
-
   void updatePlayer(entt::registry& reg) {
-    auto playerView = reg.view<Entity::Components::Speed2, Entity::Components::Movable>();
+    auto playerView = reg.view<Entity::Components::Controllable>();
     playerView.each(&updatePlayerPosition);
   }
 
-  static void updatePlayerPosition(const Entity::Components::Speed2& speed, Entity::Components::Movable& movable) {
-    playerActsByCondition(KEY_W, movable, speed, &IsKeyDown, &movePlayerUp);
-    playerActsByCondition(KEY_S, movable, speed, &IsKeyDown, &movePlayerDown);
-    playerActsByCondition(KEY_A, movable, speed, &IsKeyDown, &movePlayerLeft);
-    playerActsByCondition(KEY_D, movable, speed, &IsKeyDown, &movePlayerRight);
+private:
+  static void updatePlayerPosition(Entity::Components::Controllable& movable) {
+    playerActsByCondition(KEY_W, movable, &IsKeyDown, moveUpCommand);
+    playerActsByCondition(KEY_S, movable, &IsKeyDown, moveDownCommand);
+    playerActsByCondition(KEY_A, movable, &IsKeyDown, moveLeftCommand);
+    playerActsByCondition(KEY_D, movable, &IsKeyDown, moveRightCommand);
   }
 
-  static void movePlayerUp(Entity::Components::Movable& movable, const Entity::Components::Speed2 speed) {
-    movable.pos.y -= speed.y;
+  typedef bool (*ControllableCondition)(int key);
+  typedef void (*ControllableAction)(Entity::Components::Controllable& movable, const Entity::Components::Speed2);
+  static void playerActsByCondition(int                               key,
+                                    Entity::Components::Controllable& movable,
+                                    ControllableCondition             condition,
+                                    Commands::ICommand&               command) {
+    if (condition(key)) command.execute(movable);
   }
 
-  static void movePlayerDown(Entity::Components::Movable& movable, const Entity::Components::Speed2 speed) {
-    movable.pos.y += speed.y;
-  }
-
-  static void movePlayerLeft(Entity::Components::Movable& movable, const Entity::Components::Speed2 speed) {
-    movable.pos.x -= speed.x;
-  }
-
-  static void movePlayerRight(Entity::Components::Movable& movable, const Entity::Components::Speed2 speed) {
-    movable.pos.x += speed.x;
-  }
+  static inline Commands::MoveUpCommand    moveUpCommand;
+  static inline Commands::MoveDownCommand  moveDownCommand;
+  static inline Commands::MoveLeftCommand  moveLeftCommand;
+  static inline Commands::MoveRightCommand moveRightCommand;
 };
 
 class RenderSystem : public ISystem {
@@ -78,7 +115,7 @@ public:
     BeginDrawing();
       ClearBackground(GetColor(0x181818FF));
 
-      auto playerView = reg.view<Entity::Components::Movable, Color>();
+      entt::basic_view playerView = reg.view<Entity::Components::Controllable, Color>();
       playerView.each(&renderPlayer);
 
       DrawFPS(0, 0);
@@ -86,7 +123,7 @@ public:
   }
 
 private:
-  static void renderPlayer(const Entity::Components::Movable& movable, const Color& color) {
+  static void renderPlayer(const Entity::Components::Controllable& movable, const Color& color) {
     DrawRectangle(movable.pos.x, movable.pos.y, 50, 50, color);
   }
 };
@@ -94,14 +131,57 @@ private:
 class PhysicsSystem : public ISystem {
 public:
   void update(entt::registry& reg) {
+    MUST_IMPLEMENT();
+  }
+};
+
+class CollisionSystem : public ISystem {
+  void update(entt::registry& reg) {
+    MUST_IMPLEMENT();
   }
 };
 
 } // namespace Systems
 
+namespace Managers {
+
+class SystemManager {
+public:
+  SystemManager() {}
+
+  void updateSystems(entt::registry& reg) {
+    for (Systems::ISystem* system : systems) {
+      system->update(reg);
+    }
+  }
+
+private:
+  Systems::RenderSystem    renderSystem;
+  Systems::PhysicsSystem   physicsSystem;
+  Systems::MovementSystem  movementSystem;
+  Systems::CollisionSystem collisionSystem;
+
+private:
+  Systems::ISystem* systems[4] = {
+    &renderSystem,
+    &movementSystem,
+    &physicsSystem,
+    &collisionSystem
+  };
+};
+
+} // namespace Managers
+
 } // namespace Entity
 
 namespace Core {
+
+// @TODO: the Entt dependency must be controlled.
+//  - Create an EntityManager that encapsulates this dependency (entt) to improve system control.
+class EntityManager {
+public:
+private:
+};
 
 class Window {
 public:
@@ -111,7 +191,7 @@ public:
   }
 
   ~Window() {
-    close(); 
+    close();
   }
 
   static bool shouldNotClose() {
@@ -135,6 +215,110 @@ private:
 
 } // namespace Core
 
+struct LevelData {
+  size_t rows     = 0,
+         columns  = 0;
+  int*   ptr      = nullptr;
+};
+
+struct GameContext {
+  static inline entt::registry context;
+  static inline int            currentLevel = 1;
+
+  static LevelData getLevel(int level) {
+    switch (level) {
+      case 1:  return { LEVEL_1_ROWS, LEVEL_1_COLUMNS, *level_1 }; break;
+      case 2:  return { LEVEL_2_ROWS, LEVEL_2_COLUMNS, *level_2 }; break;
+      case 3:  return { LEVEL_3_ROWS, LEVEL_3_COLUMNS, *level_3 }; break;
+      case 4:  return { LEVEL_4_ROWS, LEVEL_4_COLUMNS, *level_4 }; break;
+      default: return {};
+    }
+    return {};
+  }
+
+  static LevelData getNextLevel() {
+    if (currentLevel == 2) { return {}; }
+    return getLevel(currentLevel++);
+  }
+
+  static LevelData getPreviousLevel() {
+    if (currentLevel == 1) { return {}; }
+    return getLevel(currentLevel--);
+  }
+
+  static LevelData getLevel() {
+    return getLevel(currentLevel);
+  }
+
+private:
+  enum {
+    LEVEL_1_ROWS    = 10,
+    LEVEL_1_COLUMNS = 10,
+
+    LEVEL_2_ROWS    = 10,
+    LEVEL_2_COLUMNS = 10,
+
+    LEVEL_3_ROWS    = 10,
+    LEVEL_3_COLUMNS = 10,
+
+    LEVEL_4_ROWS    = 10,
+    LEVEL_4_COLUMNS = 10
+  };
+
+  static inline int level_1[LEVEL_1_ROWS][LEVEL_1_COLUMNS] = {
+    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+  };
+
+  static inline int level_2[LEVEL_2_ROWS][LEVEL_2_COLUMNS] = {
+    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+  };
+
+  static inline int level_3[LEVEL_3_ROWS][LEVEL_3_COLUMNS] = {
+    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+  };
+
+  static inline int level_4[LEVEL_4_ROWS][LEVEL_4_COLUMNS] = {
+    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+  };
+};
+
+
 class Game {
 public:
   Game() {
@@ -142,39 +326,39 @@ public:
     run();
   }
 
+  ~Game() {
+    cleanUp();
+  }
+
+  /**
+   * No public member functions needed here. Just Kiss.
+   */
+
 private:
   void run() {
-    entt::registry reg;
-    initializeEntities(reg);
-
-    Entity::Systems::MovementSystem movementSystem;
-    Entity::Systems::RenderSystem   renderSystem;
-    Entity::Systems::PhysicsSystem  physicsSystem;
-
-    Entity::Systems::ISystem* systems[] = {
-      &movementSystem,
-      &renderSystem,
-      &physicsSystem
-    };
-
     while (Core::Window::shouldNotClose()) {
-      for (Entity::Systems::ISystem* system : systems) {
-        system->update(reg);
-      }
+      systemManager.updateSystems(GameContext::context);
     }
   }
 
-  void initialize() { SetTargetFPS(60); }
+  void initialize() {
+    SetTargetFPS(120);
+    initializeEntities(GameContext::context);
+  }
 
   void initializeEntities(entt::registry& reg) {
     entt::entity player = reg.create();
-    reg.emplace<Entity::Components::Speed2>(player, 6.0f, 6.0f);
-    reg.emplace<Entity::Components::Movable>(player, Vector2 { 10.f, 10.f });
+    reg.emplace<Entity::Components::Controllable>(player, Vector2 { 10.f, 10.f }, Entity::Components::Speed2 { 3.f, 3.f });
     reg.emplace<Color>(player, RED);
   }
 
+  void cleanUp() {
+    /** Future shader cleanup and stuff or some context shit cleanup */
+  }
+
 private:
-  Core::Window window{};
+  Core::Window                    window;
+  Entity::Managers::SystemManager systemManager;
 };
 
 } // namespace Cooper
